38951680593569e1cd2edd1724b91046f58588a8
AmanAntinoDev <aman.g@antino.com>
2026-02-13 11:05:17 +0530

    feat: add login button functionality and styles to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
 import { ShoppingCart, PhoneCall } from 'lucide-react';
 import { LOGO_URL } from '../utils/constant';
+import { useEffect, useState } from 'react'
 function HeaderComponent() {
+    const [btnName, setBtnName] = useState('Login');
+    function handleLoginButton() {
+        btnName === 'Login' ? setBtnName('Logout') : setBtnName('Login');
+    }
+    console.log("Render whole react component")
     return (
         <div className="header">
             <div className="logo-container">
@@ -12,6 +18,7 @@ function HeaderComponent() {
                     <li>About</li>
                     <li><ShoppingCart size={19} color='red' /> Cart</li>
                     <li><PhoneCall size={19} color='red' /> Contact</li>
+                    <button className='login-btn' onClick={handleLoginButton}>{btnName}</button>
                 </ul>
             </div>
 
